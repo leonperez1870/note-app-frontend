0b7b80a63e680bd4f41254a0a089a22733c8d31b
leonperez1870 <leo@parkfieldcommerce.com>
2019-05-22 19:27:04 -0400

    init commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# navigate into our frontend directory
+cd source/
+
+# build our frontend project
+npm install
+npm run build
+
+# navigate into the directory which contains
+# our built frontend
+cd dist/
